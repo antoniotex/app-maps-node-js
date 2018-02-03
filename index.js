d10c4cf04e6ff806230a33fbd0b94cd572e5d3be
antoniotex <acm.skt@gmail.com>
2018-02-02 22:04:56 -0200

    Adicionado API de geolocalizacao do Google Maps

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var restify = require('restify');
 
+var googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyCOAf0RvK9GhIN50iXDTMCrxwhwy1kDt7k',
+  Promise: Promise
+});
+
 var knex = require('knex')({
   client: 'mysql',
   connection: {
     host : '127.0.0.1',
     user : 'root',
     password : '',
-    database : 'maps'
+    database : 'mapsdb'
   }
 });
 
@@ -22,9 +27,21 @@ server.use(restify.plugins.bodyParser());
 server.get('/all', function (req, res, next) {
   knex('places').then((dados) =>{
     res.send(dados);
-  }, next)
+  }, next);
 
-  return netx();
+  return next();
+});
+
+server.get("/geocode", function(req, res, next){
+  googleMapsClient.geocode({address: '1600 Amphitheatre Parkway, Mountain View, CA'}).asPromise()
+  .then((response) => {
+    const address = response.json.results[0].formatted_address;
+    const place_id = response.json.results[0].place_id;
+    res.send({place_id, address});
+  })
+  .catch((err) => {
+    res.send(err);
+  });
 });
 
 server.listen(8080, function () {
